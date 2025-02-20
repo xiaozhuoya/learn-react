4103ec33ca9db4a39b405779a110eb2ab85c0161
JieZhuo <1042585668@qq.com>
2025-02-20 16:32:52 +0800

    ✨ feat: Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Button } from "antd";
+// src/App.tsx
+import React from 'react';
+import RouteConfig from '@/routes';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button type="primary" danger onClick={() => setCount(() => count + 1)}>
-          count is {count}
-        </Button>
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {/* 渲染路由配置组件，开启路由功能 */}
+      <RouteConfig />
+    </div>
   );
-}
+};
 
 export default App;
